936d510715dfe093c80c0d8ae395c21e7cf494af
That is me <trab.die@gmail.com>
2021-12-11 18:30:12 -0500

    client class initialisation

--- src/main/java/models/Client.java
+++ src/main/java/models/Client.java
@@ -1,2 +1,14 @@
-package models;public class Client {
+package models;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+
+public class Client {
+    private long client_id;
+    private int ss_number;
+    private String first_name;
+    private String last_name;
+    private String address;
+    private String phone_number;
+    private double fee_paid;
+
 }
